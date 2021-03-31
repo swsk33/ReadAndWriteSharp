e034183e042a241935dd8599a9b54c0ba5cdec5e
swsk33 <yanhanhui2001a@163.com>
2021-03-31 15:22:00 +0800

    v1.0.0

---
+++ MainTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using Swsk33.ReadAndWriteSharp;
+using System;
+using System.Text;
+
+namespace MainTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TextReader.GetFileLine(@"E:\中转\a.txt"));
+        }
+    }
+}
