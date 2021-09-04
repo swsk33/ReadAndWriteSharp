3084bf314dad63515548c15097063f63982d3de3
守望时空33 <yanhanhui2001a@163.com>
2021-09-04 20:43:47 +0800

    v2.3.0

--- MainTest/Program.cs
+++ MainTest/Program.cs
@@ -10,7 +10,6 @@ namespace MainTest
 	{
 		static void Main(string[] args)
 		{
-
 		}
 	}
 }
--- ReadAndWriteSharp/NetworkUtils.cs
+++ ReadAndWriteSharp/NetworkUtils.cs
@@ -10,6 +10,7 @@ namespace Swsk33.ReadAndWriteSharp
 	/// </summary>
 	public class NetworkUtils
 	{
+
 		/// <summary>
 		/// 发送GET请求
 		/// </summary>
@@ -44,51 +45,6 @@ namespace Swsk33.ReadAndWriteSharp
 			request.Method = "GET";
 			request.Referer = urlReferer;
 			request.Host = urlHost;
-			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-			Stream responseStream = response.GetResponseStream();
-			StreamReader streamReader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
-			string result = streamReader.ReadToEnd();
-			streamReader.Close();
-			responseStream.Close();
-			return result;
-		}
-
-		/// <summary>
-		/// 以一个特定的User-Agent发送GET请求
-		/// </summary>
-		/// <param name="url">请求地址</param>
-		/// <param name="userAgent">User-Agent，可以使用Swsk33.ReadAndWriteSharp.Param下的UserAgentValue中的常量值也可以自定义</param>
-		/// <returns>响应内容</returns>
-		public static string SendGetRequest(string url, string userAgent)
-		{
-			string urlProtocol; // 网址协议
-			string urlContent; // 网址内容
-			if (!url.StartsWith("http://") && !url.StartsWith("https://"))
-			{
-				urlProtocol = "http://";
-				urlContent = url;
-				url = urlProtocol + url;
-			}
-			else
-			{
-				urlProtocol = url.Substring(0, url.IndexOf("//") + 2);
-				urlContent = url.Substring(url.IndexOf("//") + 2);
-			}
-			string urlHost;
-			if (urlContent.Contains("/"))
-			{
-				urlHost = urlContent.Substring(0, urlContent.IndexOf("/"));
-			}
-			else
-			{
-				urlHost = urlContent;
-			}
-			string urlReferer = urlProtocol + urlHost + "/";
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-			request.Method = "GET";
-			request.Referer = urlReferer;
-			request.Host = urlHost;
-			request.UserAgent = userAgent;
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 			Stream responseStream = response.GetResponseStream();
 			StreamReader streamReader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
@@ -177,59 +133,6 @@ namespace Swsk33.ReadAndWriteSharp
 		}
 
 		/// <summary>
-		/// 以一个特定的User-Agent发送POST请求
-		/// </summary>
-		/// <param name="url">请求地址</param>
-		/// <param name="userAgent">User-Agent，可以使用Swsk33.ReadAndWriteSharp.Param下的UserAgentValue中的常量值也可以自定义</param>
-		/// <param name="contentType">请求数据类型，可以使用Swsk33.ReadAndWriteSharp.Param下的中的ContentTypeValue类的常量值</param>
-		/// <param name="requestBody">请求体，注意的是不同的内容类型有不同的请求体格式，例如application/x-www-form-urlencoded中请求体通常是：键1=值1&amp;键2=值2&amp;...</param>
-		/// <returns>响应内容</returns>
-		public static string SendPostRequest(string url, string userAgent, string contentType, string requestBody)
-		{
-			string urlProtocol; // 网址协议
-			string urlContent; // 网址内容
-			if (!url.StartsWith("http://") && !url.StartsWith("https://"))
-			{
-				urlProtocol = "http://";
-				urlContent = url;
-				url = urlProtocol + url;
-			}
-			else
-			{
-				urlProtocol = url.Substring(0, url.IndexOf("//") + 2);
-				urlContent = url.Substring(url.IndexOf("//") + 2);
-			}
-			string urlHost;
-			if (urlContent.Contains("/"))
-			{
-				urlHost = urlContent.Substring(0, urlContent.IndexOf("/"));
-			}
-			else
-			{
-				urlHost = urlContent;
-			}
-			string urlReferer = urlProtocol + urlHost + "/";
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-			request.Method = "POST";
-			request.Referer = urlReferer;
-			request.Host = urlHost;
-			request.ContentType = contentType;
-			request.UserAgent = userAgent;
-			byte[] data = Encoding.UTF8.GetBytes(requestBody);
-			using (Stream stream = request.GetRequestStream())
-			{
-				stream.Write(data, 0, data.Length);
-			}
-			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-			Stream responseStream = response.GetResponseStream();
-			StreamReader streamReader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
-			string result = streamReader.ReadToEnd();
-			streamReader.Close();
-			responseStream.Close();
-			return result;
-		}
-
-		/// <summary>
 		/// 发送自定义请求头的POST请求
 		/// </summary>
 		/// <param name="url">请求地址</param>
@@ -261,6 +164,8 @@ namespace Swsk33.ReadAndWriteSharp
 			responseStream.Close();
 			return result;
 		}
+
+
 
 		/// <summary>
 		/// 下载文件
@@ -297,57 +202,6 @@ namespace Swsk33.ReadAndWriteSharp
 			request.Method = "GET";
 			request.Referer = urlReferer;
 			request.Host = urlHost;
-			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-			Stream responseStream = response.GetResponseStream();
-			Stream fileStream = new FileStream(filePath, FileMode.Create);
-			byte[] dataByte = new byte[1024];
-			int size;
-			while ((size = responseStream.Read(dataByte, 0, dataByte.Length)) > 0)
-			{
-				fileStream.Write(dataByte, 0, size);
-			}
-			responseStream.Close();
-			fileStream.Close();
-			return File.Exists(filePath);
-		}
-
-		/// <summary>
-		/// 以一个特定的User-Agent下载文件
-		/// </summary>
-		/// <param name="url">下载地址</param>
-		/// <param name="userAgent">User-Agent，可以使用Swsk33.ReadAndWriteSharp.Param下的UserAgentValue中的常量值也可以自定义</param>
-		/// <param name="filePath">文件保存位置，已存在将覆盖</param>
-		/// <returns>是否下载成功</returns>
-		public static bool DownloadFile(string url, string userAgent, string filePath)
-		{
-			string urlProtocol; // 网址协议
-			string urlContent; // 网址内容
-			if (!url.StartsWith("http://") && !url.StartsWith("https://"))
-			{
-				urlProtocol = "http://";
-				urlContent = url;
-				url = urlProtocol + url;
-			}
-			else
-			{
-				urlProtocol = url.Substring(0, url.IndexOf("//") + 2);
-				urlContent = url.Substring(url.IndexOf("//") + 2);
-			}
-			string urlHost;
-			if (urlContent.Contains("/"))
-			{
-				urlHost = urlContent.Substring(0, urlContent.IndexOf("/"));
-			}
-			else
-			{
-				urlHost = urlContent;
-			}
-			string urlReferer = urlProtocol + urlHost + "/";
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-			request.Method = "GET";
-			request.Referer = urlReferer;
-			request.Host = urlHost;
-			request.UserAgent = userAgent;
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 			Stream responseStream = response.GetResponseStream();
 			Stream fileStream = new FileStream(filePath, FileMode.Create);
